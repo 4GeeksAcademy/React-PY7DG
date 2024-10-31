d451e9b95fb82dfe364cba8e8e356cd59604f76e
DianisMartinez <dianagomez192002@gmail.com>
2024-10-31 00:29:09 +0000

    hola

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SecondsCounter = ({ seconds }) => {
+  return (
+    <div className="Container-fluid fa-lg">
+      <div className="row bg-dark text-white p-5 mx-auto">
+        <div className="col">
+        <i className="fa-regular fa-clock"></i>
+        </div>
+        <div className="col">
+            0
+        </div>
+        <div className="col">0</div>
+        <div className="col">0</div>
+        <div className="col">0</div>
+        <div className="col">{Math.floor(seconds / 10) % 10}</div>
+        <div className="col">
+        {Math.floor(seconds / 1) % 10}</div>
+      </div>
+    </div>
+  );
+};
+export default SecondsCounter;
